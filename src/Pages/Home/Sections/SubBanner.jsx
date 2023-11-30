ad83568c22d0548ca899f50a62b200f0f648d494
apurbahasanj <apurbahasanj@gmail.com>
2023-11-30 22:18:11 +0600

    social sign in working

--- src/Pages/Home/Sections/SubBanner.jsx
+++ src/Pages/Home/Sections/SubBanner.jsx
@@ -1,7 +1,11 @@
 const SubBanner = () => {
   return (
     <section className="my-container mt-24 relative flex justify-center items-center">
-      <img className="h-full w-full object-cover" src="https://i.postimg.cc/DwG5DpZH/chef-service.jpg" alt="" />
+      <img
+        className="h-full w-full object-cover"
+        src="https://i.postimg.cc/DwG5DpZH/chef-service.jpg"
+        alt=""
+      />
       <div className="bg-white text-center bg-opacity-70 p-6 sm:p-10 md:p-14 lg:p-20 absolute w-full sm:w-4/6">
         <h2 className="text-3xl mb-2 font-serif">BISTRO BOSS</h2>
         <p>
--- src/Pages/Shared/SocialSignup/SocialSignup.jsx
+++ src/Pages/Shared/SocialSignup/SocialSignup.jsx
@@ -1,23 +1,104 @@
-
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { AuthContext } from "../../../Providers/AuthProvider";
+import { useContext } from "react";
+import Swal from "sweetalert2";
 
 const SocialSignup = () => {
-    return (
-        <div
-                //   onClick={googleSignIn}
-                className=" w-full  flex gap-8 justify-center items-center btn-sec">
-                <div className="border-gray-700 p-2 hover:bg-[#d1a054b3] hover:text-white rounded-full border-2">
-                  <FaFacebookF className="w-6 h-6" />
-                </div>
-                <div className="border-gray-700 p-2 hover:bg-[#d1a054b3] hover:text-white rounded-full border-2">
-                  <BsGoogle className="w-6 h-6" />
-                </div>
-                <div className="border-gray-700 p-2 hover:bg-[#d1a054b3] hover:text-white rounded-full border-2">
-                  <BsGithub className="w-6 h-6" />
-                </div>
-              </div>
-    );
+  const { continueWithGoogle, continueWithFacebook, continueWithGithub } =
+    useContext(AuthContext);
+
+  // google sign up
+  const handleGoogleSignIn = () => {
+    continueWithGoogle()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Sign in Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Error login user",
+          text: "Please try again.",
+        });
+      });
+  };
+
+  // continue with github
+  const handleGitHubSignIn = () => {
+    continueWithGithub()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Sign in Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Error login user",
+          text: "Please try again.",
+        });
+      });
+  };
+
+  // Continue with facebook login
+  const handleFacebookSignIn = () => {
+    continueWithFacebook()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Sign in Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Error login user",
+          text: "Please try again.",
+        });
+      });
+  };
+
+  return (
+    <div className=" w-full  flex gap-8 justify-center items-center btn-sec">
+      <button
+        onClick={handleFacebookSignIn}
+        className="border-gray-700 p-2 hover:bg-[#d1a054b3] hover:text-white rounded-full border-2">
+        <FaFacebookF className="w-6 h-6" />
+      </button>
+      <button
+        onClick={handleGoogleSignIn}
+        className="border-gray-700 p-2 hover:bg-[#d1a054b3] hover:text-white rounded-full border-2">
+        <BsGoogle className="w-6 h-6" />
+      </button>
+      <button
+        onClick={handleGitHubSignIn}
+        className="border-gray-700 p-2 hover:bg-[#d1a054b3] hover:text-white rounded-full border-2">
+        <BsGithub className="w-6 h-6" />
+      </button>
+    </div>
+  );
 };
 
 export default SocialSignup;
